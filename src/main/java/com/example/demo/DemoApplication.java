a9ed0d460ac16479eafa075b816873fd613a0ca1
CodeLuminary <victorijoni@yahoo.com>
2023-02-23 08:35:51 +0100

    Get request added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,9 +28,11 @@ public class DemoApplication {
 
 	@RequestMapping("/topics")
 	public List<TopicList> topics(){
-		return Arrays.asList(
-				new TopicList("1", "OOP", "Object oriented programming"),
-				new TopicList("2", "Functional Programming", "Introduction to functional programming")
-		);
+		return topicService.getAllTopics();
+	}
+
+	@RequestMapping("/topics/{id}")
+	public TopicList getTopic(@PathVariable String id){
+		return topicService.getTopic(id);
 	}
 }
--- src/main/java/com/example/demo/TopicService.java
+++ src/main/java/com/example/demo/TopicService.java
@@ -2,6 +2,21 @@ package com.example.demo;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Service
 public class TopicService {
+    private List<TopicList> topics = Arrays.asList(
+            new TopicList("1", "OOP", "Object oriented programming"),
+				new TopicList("2", "Functional Programming", "Introduction to functional programming")
+		);
+
+	public List<TopicList> getAllTopics(){
+		return topics;
+	}
+
+	public TopicList getTopic(String id){
+		return topics.stream().filter(x -> x.getId().equals(id)).findFirst().get();
+	}
 }
